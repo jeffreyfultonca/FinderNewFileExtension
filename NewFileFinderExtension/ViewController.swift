f4e5c91165f9a564a277bef4a79c08faecf91d1e
Jeffrey Fulton <jeffrey.fulton@me.com>
2017-06-12 16:39:53 -0500

    Initial Commit

---
+++ NewFileFinderExtension/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NewFileFinderExtension
+//
+//  Created by Jeffrey Fulton on 2017-06-12.
+//  Copyright © 2017 Jeffrey Fulton. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
